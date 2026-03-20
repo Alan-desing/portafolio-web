c0489e5c438fc89f17b0b4be4dbc8d53fa21e9f3
alan-desing <alanbissio@gmail.com>
2026-03-20 01:32:36 -0300

    Estructura inicial y navbar

---
+++ javascript/menu.js
@@ -0,0 +1,7 @@
+"js para que con el boton de hamburguesa aparezca el navbar en movil"
+const boton = document.getElementById("btn-menu");
+const menu = document.getElementById("menu");
+
+boton.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
